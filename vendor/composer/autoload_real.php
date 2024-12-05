bebd76816275a5ae98f1c6c4dc1c7d6454e8df75
JiveDig <jivetheuniversal@gmail.com>
2024-12-05 10:51:12 -0500

    1.2.2 Updated the updater

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit5401975cd875e5d77238356791b36b17
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit5401975cd875e5d77238356791b36b17::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire5401975cd875e5d77238356791b36b17($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5401975cd875e5d77238356791b36b17::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire5401975cd875e5d77238356791b36b17($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
